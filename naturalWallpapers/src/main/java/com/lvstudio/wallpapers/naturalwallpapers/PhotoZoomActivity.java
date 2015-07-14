499b2b42d2d984d74a1f0e69384a9876bacba7d1
Binh Huynh <huynhbinh0511@gmail.com>
2015-07-14 21:30:20 +0700

    commiitttt

--- naturalWallpapers/src/main/java/com/lvstudio/wallpapers/naturalwallpapers/PhotoZoomActivity.java
+++ naturalWallpapers/src/main/java/com/lvstudio/wallpapers/naturalwallpapers/PhotoZoomActivity.java
@@ -13,50 +13,57 @@ import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 import com.polites.android.GestureImageView;
 
-public class PhotoZoomActivity extends Activity {
+public class PhotoZoomActivity extends Activity
+{
 
-	private GestureImageView imgV;
-	protected ImageLoader imageLoader = ImageLoader.getInstance();
-	ProgressDialog progressDialog;
-	private DisplayImageOptions options;
+    private GestureImageView imgV;
+    protected ImageLoader imageLoader = ImageLoader.getInstance();
+    ProgressDialog progressDialog;
+    private DisplayImageOptions options;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.fragment_photozoom);
-		String uri = getIntent().getExtras().getString("URL");
-		options = new DisplayImageOptions.Builder()
-				.imageScaleType(ImageScaleType.EXACTLY).cacheInMemory(false)
-				.cacheOnDisk(true).bitmapConfig(Bitmap.Config.ARGB_8888)
-				.build();
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_photozoom);
+        String uri = getIntent().getExtras().getString("URL");
+        options = new DisplayImageOptions.Builder()
+                .imageScaleType(ImageScaleType.EXACTLY).cacheInMemory(false)
+                .cacheOnDisk(true).showImageOnFail(R.drawable.load_fail).bitmapConfig(Bitmap.Config.ARGB_8888)
+                .build();
 
-		imgV = (GestureImageView) findViewById(R.id.imageV);
+        imgV = (GestureImageView) findViewById(R.id.imageV);
 
-		imageLoader.loadImage(uri, options, new ImageLoadingListener() {
+        imageLoader.loadImage(uri, options, new ImageLoadingListener()
+        {
 
-			@Override
-			public void onLoadingStarted(String arg0, View arg1) {
-				progressDialog = ProgressDialog.show(PhotoZoomActivity.this,
-						null, "Loading....");
-			}
+            @Override
+            public void onLoadingStarted(String arg0, View arg1)
+            {
+                progressDialog = ProgressDialog.show(PhotoZoomActivity.this,
+                        null, "Loading....");
+            }
 
-			@Override
-			public void onLoadingFailed(String arg0, View arg1, FailReason arg2) {
-				progressDialog.dismiss();
-			}
+            @Override
+            public void onLoadingFailed(String arg0, View arg1, FailReason arg2)
+            {
+                progressDialog.dismiss();
+            }
 
-			@Override
-			public void onLoadingComplete(String arg0, View arg1, Bitmap arg2) {
-				progressDialog.dismiss();
-				imgV.setImageBitmap(arg2);
-			}
+            @Override
+            public void onLoadingComplete(String arg0, View arg1, Bitmap arg2)
+            {
+                progressDialog.dismiss();
+                imgV.setImageBitmap(arg2);
+            }
 
-			@Override
-			public void onLoadingCancelled(String arg0, View arg1) {
-				progressDialog.dismiss();
-			}
-		});
+            @Override
+            public void onLoadingCancelled(String arg0, View arg1)
+            {
+                progressDialog.dismiss();
+            }
+        });
 
-	}
+    }
 
 }
